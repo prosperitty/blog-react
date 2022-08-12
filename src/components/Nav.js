d3d37ac958d72ca1bf2456a60e6759a13f3f5796
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-08-12 01:29:53 -0400

    added dynamic links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,28 +4,51 @@ import { AuthContext } from '../Context/AuthContext';
 
 function Nav() {
   const { isAuthenticated } = useContext(AuthContext);
-  return (
-    <nav className="nav">
-      <div className="logo">Blog</div>
-      <div>
-        <Link className="nav-link" to="/">
-          home
-        </Link>
-        <Link className="nav-link" to="blogs">
-          blogs
-        </Link>
-        <Link className="nav-link" to="login">
-          login
-        </Link>
-        <Link className="nav-link" to="signup">
-          signup
-        </Link>
-        <Link className="nav-link" to="blogs/create">
-          new article
-        </Link>
-      </div>
-    </nav>
-  );
+
+  if (!isAuthenticated) {
+    return (
+      <nav className="nav">
+        <div className="logo">Blog</div>
+        <div>
+          <Link className="nav-link" to="/">
+            home
+          </Link>
+          <Link className="nav-link" to="blogs">
+            blogs
+          </Link>
+          <Link className="nav-link" to="login">
+            login
+          </Link>
+          <Link className="nav-link" to="signup">
+            signup
+          </Link>
+          <Link className="nav-link" to="blogs/create">
+            new article
+          </Link>
+        </div>
+      </nav>
+    );
+  } else {
+    return (
+      <nav className="nav">
+        <div className="logo">Blog</div>
+        <div>
+          <Link className="nav-link" to="/">
+            home
+          </Link>
+          <Link className="nav-link" to="blogs">
+            blogs
+          </Link>
+          <Link className="nav-link" to="blogs/create">
+            new article
+          </Link>
+          <Link className="nav-link" to="users/profile">
+            profile
+          </Link>
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default Nav;
