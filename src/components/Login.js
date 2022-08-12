23c2cf2e6f797096befe3acf5a51de0c9bbc7f01
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-08-12 01:08:00 -0400

    added loading and authentication conditionals

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,40 @@
 import { useEffect, useState } from 'react';
 import logo from '../logo.svg';
 import '../App.css';
+import { Navigate } from 'react-router-dom';
 
 function Login() {
-  const [apiResponse, setApiResponse] = useState('');
+  const [apiResponse, setApiResponse] = useState({
+    isLoading: true,
+    isLoggedIn: false,
+  });
 
   useEffect(() => {
-    fetch("login")
-      .then(res => res.json())
-      .then(res => setApiResponse(res))
-      .catch(err => err);
-  }, [setApiResponse]);
+    fetch('login')
+      .then((res) => res.json())
+      //remove req.user object
+      .then((res) =>
+        setApiResponse({
+          isLoading: false,
+          isLoggedIn: res.isLoggedIn,
+        })
+      )
+      .catch((err) => err);
+  }, []);
 
+  if (apiResponse.isLoading) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Loading...</p>
+        </header>
+      </div>
+    );
+  } else if (apiResponse.isLoggedIn) {
+    return <Navigate to="/blogs" replace="true" />;
+  } else {
+    console.log(apiResponse);
     return (
       <div className="App">
         <header className="App-header">
@@ -28,9 +51,16 @@ function Login() {
             Learn React
           </a>
         </header>
-        <p>{apiResponse.message}</p>
+        <form action="" method="POST">
+          <label htmlFor="username">Username</label>
+          <input name="username" placeholder="username" type="email" />
+          <label htmlFor="password">Password</label>
+          <input name="password" type="password" />
+          <button>Log In</button>
+        </form>
       </div>
     );
+  }
 }
 
 export default Login;
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,17 +1,38 @@
 import { useEffect, useState } from 'react';
 import logo from '../logo.svg';
 import '../App.css';
+import { Navigate } from 'react-router-dom';
 
 function Signup() {
-  const [apiResponse, setApiResponse] = useState('');
+  const [apiResponse, setApiResponse] = useState({
+    isLoading: true,
+    isLoggedIn: false,
+  });
 
   useEffect(() => {
-    fetch("signup")
-      .then(res => res.json())
-      .then(res => setApiResponse(res))
-      .catch(err => err);
+    fetch('signup')
+      .then((res) => res.json())
+      .then((res) =>
+        setApiResponse({
+          isLoading: false,
+          isLoggedIn: res.isLoggedIn,
+        })
+      )
+      .catch((err) => err);
   }, [setApiResponse]);
 
+  if (apiResponse.isLoading) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Loading...</p>
+        </header>
+      </div>
+    );
+  } else if (apiResponse.isLoggedIn) {
+    return <Navigate to="/blogs" replace="true" />;
+  } else {
     return (
       <div className="App">
         <header className="App-header">
@@ -28,9 +49,21 @@ function Signup() {
             Learn React
           </a>
         </header>
-        <p>{apiResponse.message}</p>
+        <h1>Sign Up</h1>
+        <form action="" method="POST">
+          <label htmlFor="firstname">First Name</label>
+          <input name="firstname" placeholder="first name" type="text" />
+          <label htmlFor="lastname">Last Name</label>
+          <input name="lastname" placeholder="last name" type="text" />
+          <label htmlFor="username">Username</label>
+          <input name="username" placeholder="username" type="email" />
+          <label htmlFor="password">Password</label>
+          <input name="password" placeholder="password" type="password" />
+          <button>Sign Up</button>
+        </form>
       </div>
     );
+  }
 }
 
 export default Signup;
