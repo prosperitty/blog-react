307e301636df23b8a2739162e8b08643680483d1
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-08-16 23:56:30 -0400

    added logout componenet logic

---
+++ src/components/Logout.js
@@ -0,0 +1,39 @@
+import { useEffect, useState, useContext } from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+import { Navigate } from 'react-router-dom';
+import { AuthContext } from '../Context/AuthContext';
+import Auth from '../Context/Auth';
+
+function Logout() {
+  const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
+  const [apiResponse, setApiResponse] = useState({
+    isLoading: true
+  });
+
+  useEffect(() => {
+    Auth.logout()
+    .then((res) => {
+      if (res.statusCode === 200) {
+        setIsAuthenticated(res.isLoggedIn);
+        setApiResponse({isLoading: false})
+      }
+    })
+    .catch(err => err);
+  }, [setIsAuthenticated]);
+
+  if (apiResponse.isLoading) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Logging out...</p>
+        </header>
+      </div>
+    );
+  } else if (!isAuthenticated) {
+    return <Navigate to="/" replace="true" />;
+  }
+}
+
+export default Logout;
