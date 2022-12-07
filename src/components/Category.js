c5080ed3ddf00cca879b741b2d38aa0deff7d66b
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-12-07 03:25:02 -0500

    added class names and adjusted page to show category list

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,112 +1,46 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
 import logo from '../logo.svg';
-import { Buffer } from 'buffer';
 import '../App.css';
-import { Link, Navigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Category() {
   const [apiResponse, setApiResponse] = useState({
     category_list: [],
+    isLoading: true,
     error: undefined,
   });
-  const [isLoading, setIsLoading] = useState(true);
-  const [isEditing, setisEditing] = useState(false);
-  const [isDeleted, setIsDeleted] = useState(false);
-  const [categoryId] = useState(useParams());
-
-  useEffect(() => {
-    callAPI();
-  }, []);
 
   async function callAPI() {
     try {
-      const response = await fetch(`${categoryId.categoryId}`);
+      const response = await fetch('category');
       const res = await response.json();
       setApiResponse({
+        latest_list: res.latest_list,
         category_list: res.category_list,
-        category: res.category,
+        isLoading: false,
         error: res.error,
       });
-      setIsLoading(false);
       return;
     } catch (err) {
       console.error(err);
     }
   }
 
-  function editCategory() {
-    return setisEditing(true);
-  }
+  useEffect(() => {
+    callAPI();
+  }, []);
 
-  async function submitEdit() {
-    try {
-      setIsLoading(true);
-      const categoryName = document.querySelector('#category-name');
-      const response = await fetch(`${categoryId.categoryId}`, {
-        method: 'PUT',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ category: categoryName.value }),
-      });
-      const res = await response.json();
-      console.log('result:', res);
-      if (!response.ok) {
-        throw new Error('network response was not ok');
-      } else if (response.ok) {
-        console.log('response ok', response.status);
-        setisEditing(false);
-        return callAPI();
-      }
-    } catch (error) {
-      console.error(
-        'There has been a problem with your fetch operation:',
-        error
-      );
-    }
-  }
-
-  async function deleteCategory() {
-    try {
-      setIsLoading(true);
-      const response = await fetch(`${categoryId.categoryId}`, {
-        method: 'DELETE',
-        mode: 'cors',
-      });
-      if (!response.ok) {
-        throw new Error('network response was not ok');
-      } else if (response.ok) {
-        setIsLoading(false);
-        return setIsDeleted(true);
-      }
-    } catch (error) {
-      console.error(
-        'There has been a problem with your fetch operation:',
-        error
-      );
-    }
-  }
-
-  const posts = apiResponse.category_list.map((post, index) => {
-    let buffer = new Buffer.from(post.image.data.data).toString('base64');
-    let mimetype = post.image.contentType;
+  const categories = apiResponse.category_list.map((category, index) => {
     return (
-      <div key={post._id}>
-        <h4>
-          <Link className="nav-link" to={post.url}>
-            {post.title}
-          </Link>
-        </h4>
-        <p>{post.summary}</p>
-        <p>{post.date}</p>
-        <img alt="article" src={`data:${mimetype};base64,${buffer}`} />
+      <div key={category._id}>
+        <Link to={category.url} className="category-name">
+          {category.category}
+        </Link>
       </div>
     );
   });
 
-  if (isLoading) {
+  if (apiResponse.isLoading) {
     return (
       <div className="App">
         <header className="App-header">
@@ -115,37 +49,13 @@ function Category() {
         </header>
       </div>
     );
-  } else if (isDeleted) {
-    return <Navigate to="/blogs" replace="true" />;
-  } else if (isEditing) {
-    return (
-      <div className="App">
-        <form action="" method="">
-          <div>
-            <label htmlFor="category">Category:</label>
-            <input
-              type="text"
-              placeholder="category"
-              name="category"
-              id="category-name"
-              required
-            />
-          </div>
-          <button type="submit" onClick={submitEdit}>
-            Submit
-          </button>
-        </form>
-      </div>
-    );
   } else {
     return (
-      <div className="App">
-        <header>{apiResponse.category.category}</header>
-        <section>{posts}</section>
-        <form method="">
-          <button onClick={deleteCategory}>Delete</button>
-          <button onClick={editCategory}>edit</button>
-        </form>
+      <div className="latest-page">
+        <header>
+          <h1 className="category-heading">Category List</h1>
+        </header>
+        <section className="category-list">{categories}</section>
       </div>
     );
   }
