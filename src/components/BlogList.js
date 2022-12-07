d14acc6711ce4928ee780038827f0f49470baa59
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-12-07 03:24:02 -0500

    added class names and adjusted structure of page

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -35,19 +35,61 @@ function BlogList() {
     callAPI();
   }, []);
 
-  const posts = apiResponse.article_list.map((post, index) => {
+  function displayLatestArticle(article) {
+    let buffer = new Buffer.from(article.image.data.data).toString('base64');
+    let mimetype = article.image.contentType;
+    return (
+      <div key={article._id} className="main-article">
+        <div className="main-black-gradient">
+          <img
+            alt="article"
+            className="main-image"
+            src={`data:${mimetype};base64,${buffer}`}
+          />
+        </div>
+        <div className="latest-header-article-container">
+          <div className="latest-header-article-heading">
+            <Link className="nav-link" to={article.url}>
+              <h4>{article.title}</h4>
+            </Link>
+            <p className="article-date-user">
+              {/* {article.date_formatted} */}
+              <span className="article-user">By {article.user.username}</span>
+              <span>&#8226;</span>
+              <span className="article-date">{article.date_formatted}</span>
+            </p>{' '}
+          </div>
+          <div className="latest-header-article-summary">
+            <p className="">{article.summary}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  const posts = apiResponse.article_list.slice(1).map((post, index) => {
     let buffer = new Buffer.from(post.image.data.data).toString('base64');
     let mimetype = post.image.contentType;
     return (
       <div key={post._id}>
-        <h4>
-          <Link className="nav-link" to={post.url}>
+        <img
+          alt="article"
+          className="latest-image"
+          src={`data:${mimetype};base64,${buffer}`}
+        />
+        <p className="latest-post-date">
+          <span className="article-user">By {post.user.username}</span>
+          <span>&#8226;</span>
+          <span className="article-date">{post.date_formatted}</span>
+        </p>
+        <h4 className="latest-post-title">
+          <Link className="nav-link latest-post-title" to={post.url}>
             {post.title}
           </Link>
         </h4>
-        <p>{post.summary}</p>
-        <p>{post.date}</p>
-        <img alt="article" src={`data:${mimetype};base64,${buffer}`} />
+        <div>
+          <p className="latest-post-summary">{post.summary}</p>
+        </div>
       </div>
     );
   });
@@ -63,11 +105,17 @@ function BlogList() {
     );
   } else {
     return (
-      <div className="App">
-        <header>
-          header
+      <div className="latest-page">
+        <header className="latest-header">
+          <h1>latest</h1>
+          <p>Discover the latest articles from all categories</p>
+          {displayLatestArticle(apiResponse.article_list[0])}
         </header>
-        <section>{posts}</section>
+        <hr></hr>
+        <section className="latest-posts-container">
+          <h1>latest posts</h1>
+          <div className="latest-posts-grid">{posts}</div>
+        </section>
       </div>
     );
   }
