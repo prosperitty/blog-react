3c2e81476b98fcad78bdf86b3c6d94f7011497e4
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-12-22 01:54:21 -0500

    added dynamics if authenticated or submitting

--- src/components/CategoryForm.js
+++ src/components/CategoryForm.js
@@ -1,36 +1,113 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+import { AuthContext } from '../Context/AuthContext';
 import '../App.css';
+import logo from '../logo.svg';
 
 function CategoryForm() {
-  const [apiResponse, setApiResponse] = useState('');
+  const { isAuthenticated } = useContext(AuthContext);
+  const [apiResponse, setApiResponse] = useState({isValid: false, message: ''});
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
-    fetch('https://eventhorizon.up.railway.app/category/create', {
-      mode: 'cors',
-      credentials: 'include'
-    })
-      .then((res) => res.json())
-      .then((res) => setApiResponse(res))
-      .catch((err) => err);
+    setIsLoading(true);
+    callAPI();
   }, []);
 
-  return (
-    <div className="register-page">
-      <form className="register-container" action="https://eventhorizon.up.railway.app/category/create" method="POST">
-        <label htmlFor="category">Category</label>
-        <input
-          className="register-input"
-          type="text"
-          placeholder="category"
-          name="category"
-          required
-        />
-        <button className="register-button" type="submit">
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+  function callAPI() {
+    fetch('https://eventhorizon.up.railway.app/category/create', {
+      mode: 'cors',
+      credentials: 'include',
+    })
+      .then((res) => setIsLoading(false))
+      .catch((err) => err);
+  }
+
+  function submitCategory(event) {
+    event.preventDefault();
+    setIsLoading(true);
+    const formData = new FormData(event.target);
+    const data = {};
+    formData.forEach((value, key) => {
+      data[key] = value;
+    });
+    const jsonData = JSON.stringify(data);
+
+    fetch('https://eventhorizon.up.railway.app/category/create', {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonData,
+    })
+      .then((response) => response.json())
+      .then((res) => {
+        setApiResponse({
+          message: res.message,
+          isValid: res.isValid,
+        })
+        setIsLoading(false)
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  if (isLoading) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Loading...</p>
+        </header>
+      </div>
+    );
+  } else if (apiResponse.isValid) {
+    return <Navigate to="/category" replace="true" />;
+  } else if (isAuthenticated) {
+    return (
+      <div className="register-page">
+        <form
+          className="register-container"
+          action="https://eventhorizon.up.railway.app/category/create"
+          method="POST"
+          onSubmit={submitCategory}
+        >
+          <label htmlFor="category">Category</label>
+          <input
+            className="register-input"
+            type="text"
+            placeholder="category"
+            name="category"
+            required
+          />
+          <button className="register-button" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  } else {
+    return (
+      <div className="register-page">
+        <form
+          className="register-container"
+          action="https://eventhorizon.up.railway.app/category/create"
+          method="POST"
+        >
+          <label htmlFor="category">Category</label>
+          <input
+            className="register-input"
+            type="text"
+            placeholder="must sign in to add a new category"
+            name="category"
+            disabled
+          />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default CategoryForm;
