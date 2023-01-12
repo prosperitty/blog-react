109d0cd6e379d171241a3c793d27b150ad4ac777
Alex Marmolejo <alex97marmolejo@gmail.com>
2023-01-11 23:22:55 -0500

    added form validation to blog form

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -1,36 +1,88 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import logo from '../logo.svg';
 import { Editor } from '@tinymce/tinymce-react';
+import { Navigate } from 'react-router-dom';
+import { AuthContext } from '../Context/AuthContext';
 import '../App.css';
 
 function BlogForm() {
+  const { isAuthenticated } = useContext(AuthContext);
   const [apiResponse, setApiResponse] = useState({
     header: undefined,
     category_list: [],
   });
+  const [isLoading, setIsLoading] = useState(true);
+  const [isValid, setIsValid] = useState(false);
+  const [blogURL, setBlogURL] = useState('');
+  const [article, setArticle] = useState(false);
 
   useEffect(() => {
+    callAPI();
+  }, []);
+
+  function callAPI() {
     fetch('https://eventhorizon.up.railway.app/blogs/create', {
       mode: 'cors',
-      credentials: 'include'
+      credentials: 'include',
     })
       .then((res) => res.json())
-      .then((res) =>
+      .then((res) => {
         setApiResponse({
           header: res.header,
           category_list: res.category_list,
-        })
-      )
+        });
+        setIsLoading(false);
+      })
       .catch((err) => err);
-  }, []);
+  }
 
-  const categoryList = apiResponse.category_list.map((category, index) => (
-    <option key={index} value={category._id}>
-      {category.category}
-    </option>
-  ));
+  function submitArticle(event) {
+    event.preventDefault();
+    setIsLoading(true);
+    const formData = new FormData(event.target);
+    const data = {};
+    formData.forEach((value, key) => {
+      data[key] = value;
+    });
+    // const jsonData = JSON.stringify(data);
 
-  if (apiResponse.isLoading) {
+    fetch('https://eventhorizon.up.railway.app/blogs/create', {
+      method: 'POST',
+      credentials: 'include',
+      body: data,
+    })
+      .then((response) => response.json())
+      .then((res) => {
+        setIsValid(res.isValid);
+        setArticle(res.article);
+        setBlogURL(res.blogURL);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  const categoryList = apiResponse.category_list.map((category, index) => {
+    if (article.category.category === category.category) {
+      return (
+        <option key={index} value={category._id} selected={true}>
+          {category.category}
+        </option>
+      );
+    } else {
+      return (
+        <option key={index} value={category._id}>
+          {category.category}
+        </option>
+      );
+    }
+    // <option key={index} value={category._id}>
+    //   {category.category}
+    // </option>
+  });
+
+  if (isLoading) {
     return (
       <div className="App">
         <header className="App-header">
@@ -39,7 +91,9 @@ function BlogForm() {
         </header>
       </div>
     );
-  } else {
+  } else if (isValid) {
+    return <Navigate to={blogURL} replace="true" />;
+  } else if (article && isAuthenticated) {
     return (
       <div className="blog-form-page">
         <h1>{apiResponse.header}</h1>
@@ -48,6 +102,90 @@ function BlogForm() {
           action="https://eventhorizon.up.railway.app/blogs/create"
           encType="multipart/form-data"
           method="POST"
+          onSubmit={submitArticle}
+        >
+          <div>
+            <label htmlFor="title">Title:</label>
+            <input
+              className="blog-input-text"
+              type="text"
+              placeholder="article title"
+              name="title"
+              required
+              defaultValue={article.title}
+            />
+          </div>
+          <div>
+            <label htmlFor="image">Image</label>
+            <input
+              className="blog-input-text"
+              type="file"
+              name="image"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="category">Category</label>
+            <select
+              className="blog-input-select"
+              type="select"
+              placeholder="Select a Category"
+              name="category"
+              required
+            >
+              <option value="">Select A Category</option>
+              {categoryList}
+            </select>
+          </div>
+          <div>
+            <label htmlFor="summary">Summary</label>
+            <textarea
+              className="blog-input-area"
+              name="summary"
+              rows="3"
+              placeholder="summary"
+              defaultValue={article.summary}
+              required
+            ></textarea>
+          </div>
+          <div className="blog-radio">
+            <label>Publish Article?</label>
+            <input type="radio" name="isPublished" id="yes" value={true} />
+            <label className="blog-radio-label" htmlFor="yes">
+              yes
+            </label>
+            <input type="radio" name="isPublished" id="no" value={false} />
+            <label className="blog-radio-label" htmlFor="no">
+              no
+            </label>
+          </div>
+          <div>
+            <label htmlFor="content">Content</label>
+            <Editor
+              textareaName="content"
+              initialValue={article.content}
+              required
+            />
+            {/* <textarea name="content" rows="10" placeholder="content" required></textarea> */}
+          </div>
+          <div>
+            <button className="register-button blog-button" type="submit">
+              submit
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  } else if (isAuthenticated) {
+    return (
+      <div className="blog-form-page">
+        <h1>{apiResponse.header}</h1>
+        <form
+          className="blog-form"
+          action="https://eventhorizon.up.railway.app/blogs/create"
+          encType="multipart/form-data"
+          method="POST"
+          onSubmit={submitArticle}
         >
           <div>
             <label htmlFor="title">Title:</label>
@@ -106,6 +244,83 @@ function BlogForm() {
           <div>
             <label htmlFor="content">Content</label>
             <Editor textareaName="content" required />
+            {/* <textarea name="content" rows="10" placeholder="content" required></textarea> */}
+          </div>
+          <div>
+            <button className="register-button blog-button" type="submit">
+              submit
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  } else {
+    return (
+      <div className="blog-form-page">
+        <h1>{apiResponse.header}</h1>
+        <form
+          className="blog-form"
+          action="https://eventhorizon.up.railway.app/blogs/create"
+          encType="multipart/form-data"
+          method="POST"
+        >
+          <div>
+            <label htmlFor="title">Title:</label>
+            <input
+              className="blog-input-text"
+              type="text"
+              placeholder="article title"
+              name="title"
+              disabled
+              // value="article.title"
+            />
+          </div>
+          <div>
+            <label htmlFor="image">Image</label>
+            <input
+              className="blog-input-text"
+              type="file"
+              name="image"
+              disabled
+            />
+          </div>
+          <div>
+            <label htmlFor="category">Category</label>
+            <select
+              className="blog-input-select"
+              type="select"
+              placeholder="Select a Category"
+              name="category"
+              disabled
+            >
+              <option value="">Select A Category</option>
+              {categoryList}
+            </select>
+          </div>
+          <div>
+            <label htmlFor="summary">Summary</label>
+            <textarea
+              className="blog-input-area"
+              name="summary"
+              rows="3"
+              placeholder="summary"
+              disabled
+            ></textarea>
+          </div>
+          <div className="blog-radio">
+            <label>Publish Article?</label>
+            <input type="radio" name="isPublished" id="yes" value={true} disabled />
+            <label className="blog-radio-label" htmlFor="yes">
+              yes
+            </label>
+            <input type="radio" name="isPublished" id="no" value={false} disabled />
+            <label className="blog-radio-label" htmlFor="no">
+              no
+            </label>
+          </div>
+          <div>
+            <label htmlFor="content">Content</label>
+            <Editor textareaName="content" disabled />
             {/* <textarea name="content" rows="10" placeholder="content" required></textarea> */}
           </div>
           <div>
