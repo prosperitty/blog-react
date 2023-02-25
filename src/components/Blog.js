367e88d94c67ae143be37855726fa4599301e3c5
Alex Marmolejo <alex97marmolejo@gmail.com>
2023-02-25 02:33:22 -0500

    added date field with article's original date when submitting PUT request

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -97,6 +97,7 @@ function Blog() {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
+          date: apiResponse.article.date,
           title: articleTitle,
           category: articleCategory.value,
           summary: articleSummary,
